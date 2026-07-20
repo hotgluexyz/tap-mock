932a9a1905f6a621250f155eae019fb01032e6ef
Breno <brenoss.magalhaes@gmail.com>
2026-07-20 11:22:42 -0300

    Revert "bumping hotglue-singer-sdk version to support 3.14"

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name='tap-mock',
       url='https://github.com/yourusername/tap-mock',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       install_requires=[
-          'hotglue-singer-sdk>=1.0.40',
+          'hotglue-singer-sdk>=1.0.29',
       ],
       entry_points='''
           [console_scripts]
